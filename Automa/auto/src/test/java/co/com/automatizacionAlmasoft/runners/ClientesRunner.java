39431e093b74c399e2ed2fdbf6cbe9730a6fe096
Frankool379 <franklyncasasbiuenas.904@gmail.com>
2026-06-12 00:24:12 -0500

    Restore accidentally deleted ClientesRunner

---
+++ Automa/auto/src/test/java/co/com/automatizacionAlmasoft/runners/ClientesRunner.java
@@ -0,0 +1,22 @@
+package co.com.automatizacionAlmasoft.runners;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/03_Clientes/01_AgregarCliente.feature",
+        glue = {
+                "co.com.automatizacionAlmasoft.stepsdefinitions",
+                "co.com.automatizacionAlmasoft.utils.hooks"
+        },
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports",
+                "json:target/cucumber.json"
+        },
+        monochrome = true
+)
+public class ClientesRunner {
+}
